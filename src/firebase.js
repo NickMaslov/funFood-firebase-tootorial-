eb5127e2df61a54bab8a8e2858be623058a4e11c
Mykola Maslov <kategorychnost@gmail.com>
2018-06-21 20:20:48 -0400

    init , firebase, forms

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const config = {
+  apiKey: "AIzaSyCvoxva-DhL1682W9XSgKSc1X3c5MDazbU",
+  authDomain: "fun-food-friends-97013.firebaseapp.com",
+  databaseURL: "https://fun-food-friends-97013.firebaseio.com",
+  projectId: "fun-food-friends-97013",
+  storageBucket: "fun-food-friends-97013.appspot.com",
+  messagingSenderId: "1054356912370"
+};
+firebase.initializeApp(config);
+export default firebase;
